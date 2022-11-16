f34698b7140fb2ff36e96d7d47242bb745e1bb02
piments <an.piment@terra.com.br>
2022-11-16 14:50:44 -0300

    Requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toEqual('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const list = await fetchProductsList('computador');
+    expect(list).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('verifica se a função fetchProductsList retorna um objeto igual a computadorSearch', async () => {
+    const list = await fetchProductsList('computador');   
+    expect(list).toEqual(computadorSearch);
+  });
+
+  it('verifica se a função fetchProductsList retorna erro caso não se passe arguemnto', async () => {
+    const list = await fetchProductsList('computador');     
+    expect(list).toThrow('Termo de busca não informado');
+  });  
 });
