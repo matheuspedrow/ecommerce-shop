f062e93886682b2e18d851c1b208d716e7bd537c
piments <an.piment@terra.com.br>
2022-11-16 15:53:42 -0300

    Requisito 4

--- src/main.js
+++ src/main.js
@@ -5,8 +5,19 @@ import { createProductElement } from './helpers/shopFunctions';
 
 const productShow = document.querySelector('.products');
 
+const showLoading = () => {
+  const load = document.createElement('h1');
+  load.innerText = 'carregando...';
+  load.classList.add('loading');
+  productShow.appendChild(load);
+};
+
+const hideLoading = () => document.querySelector('.loading').remove();
+
 const createList = async () => {
+  showLoading();
   const products = await fetchProductsList('computer');
+  hideLoading();
   products.forEach((product) => {
     const newProduct = createProductElement(product);
     productShow.appendChild(newProduct);
