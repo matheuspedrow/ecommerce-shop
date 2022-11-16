8299a25511965c1322849cb09df4ad29074247e7
piments <an.piment@terra.com.br>
2022-11-16 16:14:32 -0300

    Requisito 6 e correção no css

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,25 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('verifica se fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toEqual('function');
   });
+
+  it('verifica se fetchProduct chama a função fetch', () => {
+    expect(fetchProduct('MLB1405519561')).toHaveBeenCalled();
+  });  
+
+  it('verifica se fetchProduct utiliza o endpoint correto', async () => {
+    const list = await fetchProduct('MLB1405519561');
+    expect(list).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });  
+
+  it('verifica se fetchProduct retorna um produto corretamente', async () => {
+    const list = await fetchProduct('MLB1405519561');
+    expect(list).toEqual(product);
+  });  
+  it('verifica se fetchProduct retorna um erro caso não se passe parâmetro', async () => {
+    const list = await fetchProduct();
+    expect(list).toThrow('ID não informado');
+  });   
 });
