18e429f14f7d9ce825762ae48eb669093ce9c8ac
piments <an.piment@terra.com.br>
2022-11-16 15:09:12 -0300

    Requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,9 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (QUERY) => {
+  if (typeof QUERY === 'undefined') throw new Error('Termo de busca não informado');
+  const products = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+  const productlist = await products.json();
+  return productlist.results;
 };
